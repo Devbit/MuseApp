5e8236216599164f5ecb02b36d95851856bfaa67
Bart Vollebregt <bartvollebregt@hotmail.com>
2013-06-28 01:49:09 +0200

    Checking op internet connection bij launchen. Anders popup met u heeft geen internet en afsluiten.

--- src/com/hro/museapp/Launch.java
+++ src/com/hro/museapp/Launch.java
@@ -1,25 +1,56 @@
 package com.hro.museapp;
 
+import java.io.File;
+
 import com.hro.museapp.map.GPSTracker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 
 public class Launch extends Activity {
 	private ProgressDialog pDialog;
-
+	
+	private boolean noInternet() {
+	    ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+	    if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+	        return true;
+	    }
+	    return false;
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_launch);
 		getActionBar().hide();
+		if(!noInternet()) {
+			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			alertDialog.setTitle("Geen internetverbinding");
+			alertDialog.setMessage("U heeft een internetverbinding nodig om deze applicatie te gebruiken");
+			alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		        	   System.exit(0);
+		           }
+		       });
+			alertDialog.show();
+			return;
+		}
 		new BackgroundUpdate().execute();
+		
+		
 	}
-
+	
 	class BackgroundUpdate extends AsyncTask<String, String, String> {
 
 		/**
@@ -41,7 +72,11 @@ public class Launch extends Activity {
 		protected String doInBackground(String... args) {
 			CacheHandler cache = new CacheHandler(getApplicationContext());
 			GPSTracker gps = new GPSTracker(Launch.this);
+			
+			Log.d("DEBUG", "Boven de context");
 			boolean update = cache.check();
+			Log.d("DEBUG", "Onder de context");
+			
 			if (update) {
 				publishProgress();
 				cache.update();
